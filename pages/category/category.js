a49ddc1e1cf6594b614883799485d0c0ec1afa62
杨伟锋 <paranoidyang@163.com>
2018-07-15 22:14:19 +0800

    添加分类主页面侧边栏滚动

--- pages/category/category.js
+++ pages/category/category.js
@@ -90,54 +90,64 @@ Page({
         price: 18.80
       },
       {
-        id: 2,
+        id: 3,
         title: "[次日达] 特鲜炼奶饼干 芝士味 600g",
         image: "http://img.dmall.com/20160930/13dd6624-9dd8-4b98-bfaf-cbde21e8e9ae_360x360H",
         price: 6.80
       },
       {
-        id: 2,
+        id: 4,
         title: "卡夫乐之薄片饼干 原味 400g",
         image: "http://img.dmall.com/20161213/53aaae54-51de-49c0-8212-e1b4268f112d_360x360H",
         price: 8.80
       },
       {
-        id: 2,
+        id: 5,
         title: "卡夫乐之薄片饼干 原味 400g",
         image: "http://img.dmall.com/20161213/53aaae54-51de-49c0-8212-e1b4268f112d_360x360H",
         price: 8.80
       },
       {
-        id: 2,
+        id: 6,
         title: "[次日达] 特鲜炼奶饼干 芝士味 600g",
         image: "http://img.dmall.com/20160930/13dd6624-9dd8-4b98-bfaf-cbde21e8e9ae_360x360H",
         price: 8.80
       },
       {
-        id: 2,
+        id: 7,
         title: "卡夫乐之薄片饼干 原味 400g",
         image: "http://img.dmall.com/20161213/53aaae54-51de-49c0-8212-e1b4268f112d_360x360H",
         price: 8.80
       },
       {
-        id: 2,
+        id: 8,
         title: "[次日达] 特鲜炼奶饼干 芝士味 600g",
         image: "http://img.dmall.com/20160930/13dd6624-9dd8-4b98-bfaf-cbde21e8e9ae_360x360H",
         price: 8.80
       },
       {
-        id: 2,
+        id: 9,
         title: "卡夫乐之薄片饼干 原味 400g",
         image: "http://img.dmall.com/20161213/53aaae54-51de-49c0-8212-e1b4268f112d_360x360H",
         price: 8.80
       },
       {
-        id: 2,
+        id: 10,
         title: "[次日达] 特鲜炼奶饼干 芝士味 600g",
         image: "http://img.dmall.com/20160930/13dd6624-9dd8-4b98-bfaf-cbde21e8e9ae_360x360H",
         price: 8.80
       }
-    ]
+    ],
+    imageWidth: "",
+    imageHeight: "",
+    imageX: "",
+    imageY: "",
+    wxAppendData: [],
+    imageUrl: '',
+    btnId: '',
+    showImage: false,
+    imageStyle: ''
+
   },    
   /**
    * 选择商品类型
@@ -151,5 +161,71 @@ Page({
       title: event.target.id,
     })
     // 更新接口
+  },
+  /**
+   * 加入购物车
+   */
+  addToCart: function (event) {
+    // toast提示
+    wx.showToast({
+      title: '加入成功！',
+    })
+    // console.log(event.target.id)
+    // console.log(event.target.id.split(','))
+    // this.setData({
+    //   showImage: true,
+    //   imageUrl: event.target.id.split(',')[0],
+    //   btnId: event.target.id.split(',')[1]
+    // })
+    // console.log('imageUrl', this.data.imageUrl)
+    // var _this=this;
+    // /*创建节点选择器*/
+    // var query = wx.createSelectorQuery();
+    // /*选择id*/
+    // query.select('#goods-image-' + this.data.btnId).boundingClientRect()
+    // query.exec(function (res) {
+    //   /*res就是该元素的信息数组*/
+    //   console.log(res);
+    //   /*获取节点信息*/
+    //   _this.setData({
+    //     imageWidth: res[0].width,
+    //     imageHeight: res[0].height,
+    //     imageX: res[0].left,
+    //     imageY: res[0].top,
+    //     imageStyle: 'width:'+res[0].width + "px;" +
+    //                 'height:' + res[0].height + 'px;' +
+    //                 'position: fixed;' +
+    //                 'z-index: 100;' +
+    //                 'left: ' + res[0].left + 'px;' +
+    //                 'top: ' + res[0].top + 'px;' +
+    //                 'animation: drop 5s ease .5s;' +
+    //       '@-webkit-keyframes drop {\n' +
+    //     '  0% {\n' +
+    //     '    width: 60px;\n' +
+    //     '    height: 60px;\n' +
+    //     '  }\n' +
+    //     '  100% {\n' +
+    //     '    width: 1.15rem;\n' +
+    //     '    height: 1.15rem;\n' +
+    //     '    left: 200px;\n' +
+    //     '    top: 700px;\n' +
+    //     '    border-radius: 50%;\n' +
+    //     '    -webkit-transform: rotate(180deg);\n' +
+    //     '    -moz-transform: rotate(180deg);\n' +
+    //     '    -o-transform: rotate(180deg);\n' +
+    //     '    -ms-transform: rotate(180deg);\n' +
+    //     '  }\n' +
+    //     '}'
+    //
+    //   })
+    //   console.log('取坐标X', _this.data.imageX);
+    //   console.log('取坐标Y', _this.data.imageY);
+    //   /*动态渲染商品图片，用以动画效果*/
+    //   setTimeout(function () {
+    //     _this.setData({
+    //       showImage: false
+    //     })
+    //   }, 3000)
+    // })
   }
 })
